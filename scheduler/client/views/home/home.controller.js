8d18dbb2d047e9cb37edb806b421da54aaacf5ef
Evan Heisler <evanheisler@gmail.com>
2015-07-08 17:01:07 -0500

    meters rendered

--- scheduler/client/views/home/home.controller.js
+++ scheduler/client/views/home/home.controller.js
@@ -3,4 +3,36 @@
 angular.module('scheduler')
 .controller('HomeCtrl', function ($scope) {
   $scope.name = 'Scheduling App';
+
+  $scope.barWidth = function(children, occupancy, threshold) {
+    $scope.fullSections = Math.floor(children/threshold);
+    $scope.partialSection = children%threshold;
+
+    $scope.bars = [{
+      cssClass: 'progress-bar-success',
+      width: (threshold/occupancy) * 100,
+      name: 'Jane Doe'
+    }]
+    console.log('fired');
+    // return {
+    //   fullSections: fullSections,
+    //   partialSection: partialSection
+    // }
+  }
+
+  $scope.classrooms = [
+    {
+      'title': 'Classroom 1',
+      'threshold': 8,
+      'children': 25,
+      'maxOccupancy': 30
+    },
+    {
+      'title': 'Classroom 2',
+      'threshold': 12,
+      'children': 11,
+      'maxOccupancy': 28
+    }
+  ];
+
 });
